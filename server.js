a95ee8adc3d1dc63936a3e805989fc1c89b912fe
unknown <joao.souza3157@gmail.com>
2021-07-25 21:11:21 -0300

    atualizando

--- server.js
+++ server.js
@@ -1,57 +1,63 @@
 /* 
 Descricao :
-	O arquivo atualiza, adiciona e exclui dados de funcionarios, como cpf, nome, email e telefone.
+    O arquivo atualiza, adiciona e exclui dados de funcionarios, como cpf, nome, email e telefone.
 Aluno :
-	João Vitor Souza Santos
+    João Vitor Souza Santos
 Data :
-	25 / julho / 2021
+    25 / julho / 2021
 */
-
 
 const express = require('express');
 const app = express();
 const data = require("./data.json");
+const cors = require("cors");
+
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/employee", function (req, response) {
-    response.json(data);
-});
-app.get("/employee/:id", function (req, response) {
-    const { id } = req.params;
-    const employees = data.find(emp => emp.id == id);
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+    app.get("/employee", function (req, response) {
+        response.json(data);
+    });
+    app.get("/employee/:id", function (req, response) {
+        const { id } = req.params;
+        const employees = data.find(emp => emp.id == id);
 
-    if  (!employees) return response.status(204).json();
+        if (!employees) return response.status(204).json();
 
-    response.json(employees);
-});
-app.post("/employee", function (request, response) {
-    const {name, email, cpf, tel } = request.body;
+        response.json(employees);
+    });
+    app.post("/employee", function (request, response) {
+        const { name, email, cpf, tel } = request.body;
 
-    response.json({name, email, cpf, tel});
- });
-app.put("/employee/:id", function (request, response) { 
-    const { id } = request.params;
-    const employees = data.find(emp => emp.id == id);
+        response.json({ name, email, cpf, tel });
+    });
+    app.put("/employee/:id", function (request, response) {
+        const { id } = request.params;
+        const employees = data.find(emp => emp.id == id);
 
-    if  (!employees) return response.status(204).json();
+        if (!employees) return response.status(204).json();
 
-    const {name, email, cpf, tel } = request.body;
+        const { name, email, cpf, tel } = request.body;
 
-    employees.name = name;
-    employees.email = email;
-    employees.cpf = cpf;
-    employees.tel = tel;
+        employees.name = name;
+        employees.email = email;
+        employees.cpf = cpf;
+        employees.tel = tel;
 
-    response.json(employees);
-});
-app.delete("/employee/:id", function (req, response) {
-    const { id } = req.params;
-    const employeeFiltered = data.filter(employees => employees.id != id);
+        response.json(employees);
+    });
+    app.delete("/employee/:id", function (req, response) {
+        const { id } = req.params;
+        const employeeFiltered = data.filter(employees => employees.id != id);
 
-    response.json(employeeFiltered);
- });
+        response.json(employeeFiltered);
+    });
 
-app.listen(3110, function () {
-    console.log("server is running");
-});
+    app.listen(process.env.PORT || 3110, function () {
+        console.log("server is running");
+    });
